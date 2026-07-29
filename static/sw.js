1c963166bbf752a205a6eb06294ba7bb1fa60704
mediocareworkpulse <mediocareworkpulse@gmail.com>
2026-07-29 19:48:44 +0300

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'mediocare-v3';
+const ASSETS = [
+  '/',
+  '/static/style.css',
+  '/static/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
